60285f59ef5b26d19c8361a43d679ea52df1b8ec
rafastack <divengine.solutions@gmail.com>
2017-08-24 10:05:45 -0400

    update

--- build.bat
+++ build.bat
@@ -1,10 +1,18 @@
+call div get html/scripts-list.tpl
 call div get unify/page.tpl
 call div get unify/landing-row-3-cols-teasers
 call div get unify/landing-row-4-cols-thumbs
 call div get unify/box-pricing-mega.tpl
-call div build -t ./.div/templates/index.tpl -d ./.div/models/index.json -o ./index.html
-call div build -t ./.div/templates/about.tpl -d ./.div/models/index.json -o ./about.html
-call div build -t ./.div/templates/contact.tpl -d ./.div/models/index.json -o ./contact.html
+
+call div build -t ./.div/templates/index.en.tpl -d ./.div/models/index.json -o ./index.html
+call div build -t ./.div/templates/index.es.tpl -d ./.div/models/index.json -o ./es/index.html
+
+call div build -t ./.div/templates/about.en.tpl -d ./.div/models/index.json -o ./about.html
+call div build -t ./.div/templates/about.es.tpl -d ./.div/models/index.json -o ./es/about.html
+
+call div build -t ./.div/templates/contact.en.tpl -d ./.div/models/index.json -o ./contact.html
+call div build -t ./.div/templates/contact.es.tpl -d ./.div/models/index.json -o ./es/contact.html
+
 call div build -t ./.div/templates/faq.tpl -d ./.div/models/index.json -o ./faq.html
 call div build -t ./.div/templates/login.tpl -d ./.div/models/index.json -o ./login.html
 call div build -t ./.div/templates/pricing.tpl -d ./.div/models/index.json -o ./pricing.html
